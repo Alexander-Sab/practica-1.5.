8305047f224bb4f2870090ca123484d361ac7488
Alexander-Sab <113023875+Alexander-Sab@users.noreply.github.com>
2023-01-10 14:31:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+new Swiper(".contener-slide", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // Количество слайдов для показа
+  slidesPerView: 1.5,
+
+  // отступ между слайдами
+  spaceBetween: 16,
+});
